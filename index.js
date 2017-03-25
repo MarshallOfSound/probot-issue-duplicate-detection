0b4e155c6a3317866305495cb587fd8e48938a67
Samuel Attard <samuel.r.attard@gmail.com>
2017-03-26 01:40:11 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const Fuze = require('fuse.js');
+
+module.exports = (robot) => {
+  robot.on('issues.opened', async (event, context) => {
+    const github = await robot.auth(event.payload.installation.id);
+
+    // Get all issues that aren't the new issue
+    const allIssues = await github.issues.getForRepo(context.repo());
+    const otherIssues = allIssues.filter(issue => issue.number !== context.issue().number);
+    const theIssue = allIssues.find(issue => issue.number === context.issue().number);
+    
+    const fuse = new Fuze(otherIssues, {
+      shouldSort: true,
+      threshold: 0.2,
+      keys: [{
+        name: 'title',
+        weight: 0.8
+      }, {
+        name: 'body',
+        weight: 0.2
+      }]
+    });
+
+    const results = fuse.search(theIssue.title);
+    if (results.length) {
+      const commentBody = 
+`Hey There,
+We did a quick check and this issue looks very darn similar to 
+${results.slice(0, 3).map(
+  (issue, index) => `* [#${issue.number} - ${issue.title}](${issue.number})`
+).join('  \n')}
+
+This could be a cooincidence, but if any of these issues solves your problem then I did a good job :smile:
+
+If not, the maintainers will get to this issue shortly.
+
+Cheers,
+Your Friendly Neighborhood ProBot`
+
+      await github.issues.createComment(context.issue({ body: commentBody }));
+    }
+  })
+};
